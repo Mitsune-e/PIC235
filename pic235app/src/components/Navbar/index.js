64f8ea18fbd719e82bc0addb430b709deee74504
Mitsune-e <gveloso1414@gmail.com>
2023-05-29 17:53:04 -0300

    Implementing new sign in screen and nav bar adjusts

--- pic235app/src/components/Navbar/index.js
+++ pic235app/src/components/Navbar/index.js
@@ -1,35 +1,28 @@
 import React from "react";
+import "./index.css"
 
 export const Navbar = () => {
 
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary border">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">Navbar</a>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/Cadastro">Registre-se</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
+    <div className="navbar-box">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary border">
+        <div className="container-fluid">
+          <img class="logo img-fluid d-inline-block align-text-top navbar-brand" src="/DFBEagle.png" alt="Capital Digital" />
+          <div className="register-option" >
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="/Cadastro">Registre-se</a>
+              </li>
+              <li className="nav-item">
+                <div className="login-option">
+                  <a className="btn btn-outline-teal" href="/">Login</a>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   )
 }
