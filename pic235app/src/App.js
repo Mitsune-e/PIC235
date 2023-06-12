726432ca7b2ba399f77f3c381512ff69153637e6
Joao-MarcosOM <joaomarcos.bigsan@gmail.com>
2023-06-12 11:54:07 -0300

    Add notificacoes, perfil and credenciamento pages

--- pic235app/src/App.js
+++ pic235app/src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import React from "react";
 import { Home, Cadastro, Login, CadastroEmpresa } from "./pages";
 
 function App() {
@@ -9,7 +8,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/cadastroEmpresa" element={<CadastroEmpresa />} />
+        <Route path="/notificacoes" element={<Notificacoes />} />
+        <Route path="/perfil" element={<Perfil />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/credenciamento" element={<Credenciamento />} />
       </Routes>
     </div>
   )
