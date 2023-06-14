9e2b29c2f23eff7e80e049ea8cf378fac9e9b83d
Isabella Calazans <isabella.calazans6@gmail.com>
2023-06-14 18:20:10 -0300

    Página Custos Variados
    Página parcial de Custos Variados

--- pic235app/src/pages/CustosFixos/index.js
+++ pic235app/src/pages/CustosFixos/index.js
@@ -4,53 +4,41 @@ import "./index.css";
 
 export const CustosFixos = (props) => {
   const [custosFixos, setCustosFixos] = useState([]);
+  const [descricao, setDescricao] = useState("");
+  const [classificacao, setClassificacao] = useState("mes");
+  const [unidade, setUnidade] = useState("");
+  const [valor, setValor] = useState("");
+  const [vigenciaInicio, setVigenciaInicio] = useState("");
+  const [vigenciaFim, setVigenciaFim] = useState("");
 
-  // Função para adicionar dados simulados
-  const adicionarDadosSimulados = () => {
-    setCustosFixos([
-      {
-        id: 1,
-        descricao: "Custo Fixo 1",
-        classificacao: "Mês",
-        unidade: "Unidade 1",
-        valor: 1000,
-        vigenciaInicio: "2023-06-01",
-        vigenciaFim: "2023-06-30",
-      },
-      {
-        id: 2,
-        descricao: "Custo Fixo 2",
-        classificacao: "Mês",
-        unidade: "Unidade 2",
-        valor: 2000,
-        vigenciaInicio: "2023-07-01",
-        vigenciaFim: "2023-07-31",
-      },
-    ]);
+  // Função para adicionar um novo custo fixo
+  const adicionarCustoFixo = () => {
+    const novoCusto = {
+      id: custosFixos.length + 1,
+      descricao: descricao,
+      classificacao: classificacao,
+      unidade: unidade,
+      valor: valor,
+      vigenciaInicio: vigenciaInicio,
+      vigenciaFim: vigenciaFim,
+    };
+
+    setCustosFixos([...custosFixos, novoCusto]);
+
+    // Limpar os campos após adicionar o custo fixo
+    setDescricao("");
+    setClassificacao("mes");
+    setUnidade("");
+    setValor("");
+    setVigenciaInicio("");
+    setVigenciaFim("");
   };
 
   return (
     <div>
       <Navbar />
       <div className="container">
-        <div className="side-menu">
-          <ul>
-            <li>
-              <a href="#">Dashboard</a>
-            </li>
-            <li>
-              <a href="#">Notificações</a>
-            </li>
-            <li>
-              <a href="#" className="active">
-                Cadastro de Custos
-              </a>
-            </li>
-            <li>
-              <a href="#">Custos Fixos</a>
-            </li>
-          </ul>
-        </div>
+        
         <div className="content">
           <div className="bullets">
             <span className="bullet">1</span>
@@ -78,32 +66,67 @@ export const CustosFixos = (props) => {
           <div className="input-container">
             <div className="input-group">
               <label htmlFor="descricao">Descrição do Custo</label>
-              <input type="text" id="descricao" />
+              <input
+                type="text"
+                id="descricao"
+                value={descricao}
+                onChange={(e) => setDescricao(e.target.value)}
+              />
             </div>
+
             <div className="input-group">
               <label htmlFor="classificacao">Classificação da Unidade</label>
-              <select id="classificacao">
+              <select
+                id="classificacao"
+                value={classificacao}
+                onChange={(e) => setClassificacao(e.target.value)}
+              >
                 <option value="mes">Mês</option>
               </select>
             </div>
+
             <div className="input-group">
               <label htmlFor="unidade">Unidade</label>
-              <input type="text" id="unidade" />
+              <input
+                type="text"
+                id="unidade"
+                value={unidade}
+                onChange={(e) => setUnidade(e.target.value)}
+              />
             </div>
+
             <div className="input-group">
               <label htmlFor="valor">Valor</label>
-              <input type="number" id="valor" />
+              <input
+                type="number"
+                id="valor"
+                value={valor}
+                onChange={(e) => setValor(e.target.value)}
+              />
             </div>
+
             <div className="input-group">
               <label htmlFor="vigencia-inicio">Vigência a partir de:</label>
-              <input type="date" id="vigencia-inicio" />
+              <input
+                type="date"
+                id="vigencia-inicio"
+                value={vigenciaInicio}
+                onChange={(e) => setVigenciaInicio(e.target.value)}
+              />
             </div>
+
             <div className="input-group">
               <label htmlFor="vigencia-fim">Vigência até:</label>
-              <input type="date" id="vigencia-fim" />
+              <input
+                type="date"
+                id="vigencia-fim"
+                value={vigenciaFim}
+                onChange={(e) => setVigenciaFim(e.target.value)}
+              />
             </div>
+            
             <div className="input-group">
-              <button onClick={adicionarDadosSimulados}>Adicionar</button>
+              <button onClick={adicionarCustoFixo}>Adicionar</button>
             </div>
           </div>
 
@@ -129,7 +152,8 @@ export const CustosFixos = (props) => {
                   <td>{custo.vigenciaInicio}</td>
                   <td>{custo.vigenciaFim}</td>
                   <td>
-                    
+                    <button>Editar</button>
+                    <button>Excluir</button>
                   </td>
                 </tr>
               ))}
--- pic235app/src/pages/CustosVariados/index.js
+++ pic235app/src/pages/CustosVariados/index.js
@@ -3,27 +3,25 @@ import { Navbar } from "../../components";
 import "./index.css";
 
 export const CustosVariados = (props) => {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [isCustoVariavel, setIsCustoVariavel] = useState(false);
+
+  const handleCheckboxChange = (option) => {
+    if (selectedOptions.includes(option)) {
+      setSelectedOptions(selectedOptions.filter((item) => item !== option));
+    } else {
+      setSelectedOptions([...selectedOptions, option]);
+    }
+  };
+
+  const handleCustoVariavelChange = (value) => {
+    setIsCustoVariavel(value === "sim");
+  };
+
   return (
     <div>
       <Navbar />
       <div className="container">
-        <div className="side-menu">
-          <ul>
-            <li>
-              <a href="#">Dashboard</a>
-            </li>
-            <li>
-              <a href="#">Notificações</a>
-            </li>
-            <li>
-              <a href="#" className="active">Cadastro de Custos</a>
-            </li>
-            <li>
-              <a href="#">Custos Fixos</a>
-            </li>
-          </ul>
-        </div>
-
         <div className="content">
           <div className="bullets">
             <span className="bullet">1</span>
@@ -34,16 +32,85 @@ export const CustosVariados = (props) => {
             <hr className="bullet-line" />
             <span className="bullet">4</span>
           </div>
-        
+
           <div className="bullet-labels">
-            <span id="lblB1" className="bullet-label">Custos Fixos</span>
-            <span id="lblB2" className="bullet-label">Custos Variáveis</span>
+            <span id="lblB1" className="bullet-label">
+              Custos Fixos
+            </span>
+            <span id="lblB2" className="bullet-label">
+              Custos Variáveis
+            </span>
             <span id="lblB3" className="bullet-label">?</span>
             <span id="lblB4" className="bullet-label">Enviar Solicitação</span>
           </div>
 
+          <div className="options">
+            <div className="title">
+              <h3>Cadastrar Custos</h3>
+              <p>Qual a classificação tributária da empresa?</p>
+            </div>
+
+            <label>
+              <input
+                type="checkbox"
+                value="MEI"
+                checked={selectedOptions.includes("MEI")}
+                onChange={() => handleCheckboxChange("MEI")}
+              />
+              MEI
+            </label> <br/>
+
+            <label>
+              <input
+                type="checkbox"
+                value="Simples Nacional"
+                checked={selectedOptions.includes("Simples Nacional")}
+                onChange={() => handleCheckboxChange("Simples Nacional")}
+              />
+              Simples Nacional
+            </label> <br/>
+
+            <label>
+              <input
+                type="checkbox"
+                value="Lucro Presumido"
+                checked={selectedOptions.includes("Lucro Presumido")}
+                onChange={() => handleCheckboxChange("Lucro Presumido")}
+              />
+              Lucro Presumido
+            </label> <br/>
+
+            <label>
+              <input
+                type="checkbox"
+                value="Lucro Real"
+                checked={selectedOptions.includes("Lucro Real")}
+                onChange={() => handleCheckboxChange("Lucro Real")}
+              />  
+               Lucro Real
+            </label> <br/>
+
+            <p>Deseja Cadastrar um custo Variável?</p>
+            <div className="button-group">
+              <button
+                className={isCustoVariavel ? "active" : ""}
+                onClick={() => handleCustoVariavelChange("sim")}
+              >
+                Sim
+              </button>
+              <button
+                className={!isCustoVariavel ? "active" : ""}
+                onClick={() => handleCustoVariavelChange("não")}
+              >
+                Não
+              </button>
+
+            </div>
+          </div>
+
+          
         </div>
-        </div>
+      </div>
     </div>
   );
 };
