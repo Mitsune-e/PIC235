e7a0ed10a40621ae45b17bb8c27decb8b454d73a
Mitsune-e <gveloso1414@gmail.com>
2023-08-17 17:37:16 -0300

    New icons for Side bar with FontAwesome

--- pic235app/src/components/SideBar/index.js
+++ pic235app/src/components/SideBar/index.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./index.css";
 import { Link } from "react-router-dom";
 import { EmpresaService } from "../../services";
+import { SideBarIcon } from "./SideBarIcon";
+import { faTableColumns, faSuitcase, faPeopleGroup, faFilePen, faList, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { SideBarItem } from "./SideBarItem";
 
 
 export const SideBar = () => {
@@ -31,12 +34,12 @@ export const SideBar = () => {
           </div>
           <div className="option-row">
             <ul className="sidebar-nav">
-              <li className="sidebar-item"><Link className="sidebar-link" to="/dashboard">Dashboard</Link></li>
-              <li className="sidebar-item"><Link className="sidebar-link" to="/empresa">Perfil da Empresa</Link></li>
-              <li className="sidebar-item"><Link className="sidebar-link" to="/funcoes">Funções da Empresa</Link></li>
-              <li className="sidebar-item"><Link className="sidebar-link" to="/recurso">Cadastro de Aplicação de Recurso</Link></li>
-              <li className="sidebar-item"><Link className="sidebar-link" to="/projeto">Projeto</Link></li>
-              <li className="sidebar-item"><Link className="sidebar-link" to="/cadastro">Cadastro de Usuarios</Link></li>
+              <SideBarItem icon={faTableColumns} path={"dashboard"} title={"Dashboard"} />
+              <SideBarItem icon={faSuitcase} path={"empresa"} title={"Perfil de Empresa"} />
+              <SideBarItem icon={faPeopleGroup} path={"funcoes"} title={"Funções da Empresa"} />
+              <SideBarItem icon={faFilePen} path={"recurso"} title={"Cadastro de Aplicação de Recurso"} />
+              <SideBarItem icon={faList} path={"projeto"} title={"Projeto"} />
+              <SideBarItem icon={faUserPlus} path={"cadastro"} title={"Cadastro de Usuarios"} />
             </ul>
           </div>
           {Erro !== "" && <div className="alert alert-danger">{Erro}</div>}
